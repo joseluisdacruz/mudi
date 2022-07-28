20037306d772b5c8137ade913b24c2dcd0a39c1b
17890486804 <jose-luis.cruz@serpro.gov.br>
2022-07-28 17:32:14 -0300

    initial commit

---
+++ src/main/java/br/com/alura/mvc/mudi/model/User.java
@@ -0,0 +1,48 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	private String username;
+	
+	private String password;
+	
+	private String enabled;
+	
+	@OneToMany
+	private List<Pedido> pedidos;
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(String enabled) {
+		this.enabled = enabled;
+	}
+}
