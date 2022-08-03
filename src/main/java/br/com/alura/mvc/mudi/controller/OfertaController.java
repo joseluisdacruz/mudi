7f1aa026d54d0d62ae2a47e7eb24d54f9234783a
17890486804 <jose-luis.cruz@serpro.gov.br>
2022-08-03 09:40:33 -0300

    usando Vuejs

--- src/main/java/br/com/alura/mvc/mudi/controller/OfertaController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/OfertaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class OfertaController {
 
-	@GetMapping
+	@GetMapping("oferta")
 	public String getFormularioParaOfertas() {
 		return "oferta/home";		
 	}
